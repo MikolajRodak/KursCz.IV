ad5e813db8086b7cc18f301017996e481461f42f
Mikołaj Rodak <mrodak03@gmail.com>
2023-08-09 17:34:51 +0200

    fourth commit - starting template to build a website

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,16 @@ const uglify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
 const sourcemaps = require('gulp-sourcemaps')
 const browserSync = require('browser-sync').create()
+const clean = require('gulp-clean')
+const kit = require('gulp-kit')
 const reload = browserSync.reload
 
 const paths = {
+	html: './html/**/*.kit',
 	sass: './src/sass/**/*.scss',
 	js: './src/js/**/*.js',
 	img: './src/img/*',
+	dist: './dist',
 	sassDest: './dist/css',
 	jsDest: './dist/js',
 	imgDest: './dist/img',
@@ -66,6 +70,18 @@ function imagesMim(cb) {
 	cb()
 }
 
+function cleanStuff(cb) {
+	return src(paths.dist, { read: false }).pipe(clean())
+	cb()
+}
+
+function handleKit(cb) {
+	src(paths.html)
+	.pipe(kit())
+	.pipe(dest('./'))
+	cb()
+}
+
 function startBrowserSync(cb) {
 	browserSync.init({
 		server: {
@@ -78,11 +94,12 @@ function startBrowserSync(cb) {
 
 function watchForChanges(cb) {
 	watch('./*.html').on('change', reload)
-	watch([paths.sass, paths.js], parallel(sassCompiler, javaScript)).on('change', reload)
+	watch([paths.html, paths.sass, paths.js], parallel(handleKit, sassCompiler, javaScript)).on('change', reload)
 	watch(paths.img, imagesMim).on('change', reload)
 
 	cb()
 }
 
-const functions = parallel(sassCompiler, javaScript, imagesMim)
+const functions = parallel(handleKit,sassCompiler, javaScript, imagesMim)
+exports.cleanStuff = cleanStuff
 exports.default = series(functions, startBrowserSync, watchForChanges)
